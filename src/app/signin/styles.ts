509466f7a2b507ae2c978daeba0234271ea144e0
Yuri Na <soceu@naver.com>
2024-07-03 22:16:59 +0900

    Style: 로그인 페이지 스타일 수정

--- src/app/signin/styles.ts
+++ src/app/signin/styles.ts
@@ -6,6 +6,17 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  background: rgb(253, 150, 143);
+  background: linear-gradient(
+    0deg,
+    rgba(253, 150, 143, 1) 0%,
+    rgba(251, 104, 139, 1) 50%,
+    rgba(141, 105, 214, 1) 100%
+  );
+
+  & > img {
+    width: 75%;
+  }
 `;
 
 export const S = {
