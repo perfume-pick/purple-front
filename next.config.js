08c99bc8963179468b069bd135b50c194f5199b4
Yuri Na <soceu@naver.com>
2024-07-07 18:03:05 +0900

    Feat: 로그인 작업중

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/perpicks/auth/login/kakao/success:path*/",
+  //       destination: `http://43.202.196.141:3000/perpicks/auth/login/kakao/success:path*/`,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
--- src/app/perpicks/auth/login/[type]/page.tsx
+++ src/app/perpicks/auth/login/[type]/page.tsx
@@ -1,13 +1,9 @@
 "use client";
-// import serverHttp from "@/utils/http/serverHttp";
 import { AuthLoginTryDTO } from "@/dto/authDTO";
 import clientHttp from "@/utils/http/clientHttp";
 
-import { useSearchParams, useRouter } from "next/navigation";
-// import { useRouter } from "next/router";
 import { useEffect } from "react";
 // import { TOKEN_SAVE_KEY } from "@/constant/session";
-import TokenService from "@/utils/tokenService";
 
 type Req = {
   params: {
@@ -20,36 +16,21 @@ type Req = {
 };
 
 const KakaoCallbackPage = (req: Req) => {
-  console.log(req);
-  //   const { searchParams } = req;
-  //   const data = await serverHttp.post<never, AuthLoginTryDTO>(
-  //     `/perpicks/auth/login/${searchParams.code}`,
-  //   );
-  //   console.log(data);
-
-  const params = useSearchParams();
-  // const router = useRouter();
-
   useEffect(() => {
-    const code = params.get("code");
     const type = req.params.type;
-    if (code) {
-      // window.localStorage.setItem(TOKEN_SAVE_KEY, tokenValue);
-      //   TokenService.setToken(tokenValue);
-      const { searchParams } = req;
-      console.log(clientHttp);
+    const { searchParams } = req;
+
+    // 임시 코드
+    setTimeout(() => {
       clientHttp
         .post<
           never,
           AuthLoginTryDTO
-        >(`/perpicks/auth/login/${type.toUpperCase()}?code=${searchParams.code}`)
+        >(`/perpicks/auth/login/${type.toUpperCase()}?code=${searchParams.code}`, null)
         .then((res: any) => {
           console.log(res);
         });
-      //   return router.replace("/"); // go to main page if login success
-    } else {
-      // error case when failed login
-    }
+    }, 5000);
   }, []);
 
   return <div>카카오 로그인 중...</div>;
