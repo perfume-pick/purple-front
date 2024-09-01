e42bf479215c7bec2780d15473891ab99ed87352
Yuri Na <soceu@naver.com>
2024-09-01 15:47:25 +0900

    Feat: TokenService import 구문 변경

--- src/app/api/delete-token/route.ts
+++ src/app/api/delete-token/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import TokenServiceType from "@/utils/TokenService";
+import TokenService from "@/utils/TokenService";
 
 export async function DELETE(req: NextRequest, res: NextResponse) {
   try {
     const res = NextResponse.json(true, { status: 200 });
-    TokenServiceType.removeToken(req, res);
+    TokenService.removeToken(req, res);
 
     return res;
   } catch (error) {
--- src/app/api/set-token/route.ts
+++ src/app/api/set-token/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import TokenServiceType from "@/utils/TokenService";
+import TokenService from "@/utils/tokenService";
 
 export async function POST(req: NextRequest) {
   try {
     const { jwtToken } = await req.json();
     const res = NextResponse.json(true, { status: 200 });
 
-    TokenServiceType.setToken(jwtToken, req, res);
+    TokenService.setToken(jwtToken, req, res);
 
     return res;
   } catch (error) {
