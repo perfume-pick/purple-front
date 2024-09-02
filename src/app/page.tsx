a3f1d67c8e7916f33e2c8b4e85a6363ccd539fbe
Yuri Na <soceu@naver.com>
2024-09-02 22:47:44 +0900

    Feat: 필요없는 페이지 삭제  / 테스트 코드 에러 방지 옵셔널 체이닝 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function MainPage() {
     /* TODO: 서버사이드 api 호출 test (추후 삭제) */
   }
   const response = await getPerfumePreference();
-  console.log(response.data);
+  console.log(response?.data);
 
   return <PublicBanners />;
 }
