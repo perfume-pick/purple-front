862706c51d215b08c2958e0d1fc8c43bc89aa532
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-18 20:24:02 +0900

    style: 마이페이지 패딩, 마진 스타일 추가

--- src/app/myPage/styles.ts
+++ src/app/myPage/styles.ts
@@ -2,7 +2,8 @@ import { theme } from "@/styles/theme";
 import styled from "@emotion/styled";
 
 const Wrapper = styled.div`
-  padding: 0 1.6rem 9.7rem;
+  padding: 0 1.6rem 4.1rem;
+  margin-bottom: 9.7rem;
   background-color: ${theme.color.white};
 `;
 
