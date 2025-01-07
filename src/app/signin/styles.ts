73f6d0019330fe3265a7a4087c065a4267e96ae4
Yuri Na <soceu@naver.com>
2025-01-07 23:05:26 +0900

    Style: iOS 회색 영역 노출 이슈 대응 코드 변경

--- src/app/signin/styles.ts
+++ src/app/signin/styles.ts
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 const Wrapper = styled.div`
-  height: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
